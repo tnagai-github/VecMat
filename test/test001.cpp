f6ecaef04a9492cb69dc11c05096b938c0c0c878
Tetsuro Nagai <tnagai@nagoya-u.jp>
2023-06-01 15:58:38 +0900

    some more tests were added

--- test/test001.cpp
+++ test/test001.cpp
@@ -16,19 +16,6 @@
 
 using namespace VecMat;
 
-//
-//TEST(TestVecNd, Basics002){
-//  EXPECT_EQ(vecNd<4>(3.0), vecNd<4>(1.0)+ vecNd<4>(2.0)) ;
-//  EXPECT_EQ(vecNd<4>(-1.0), vecNd<4>(1.0)- vecNd<4>(2.0)) ;
-//  EXPECT_EQ((vecNd<2>{1,0}), (vecNd<2>{2,3} - vecNd<2>{1,3})) ;
-//}
-//
-//TEST(TestVecNd, Basics003){
-//  EXPECT_EQ(vecNd<4>(3.0), -vecNd<4>(-3.0)) ;
-//  EXPECT_EQ(vecNd<4>(1.0), -1.0*vecNd<4>(-1.0)) ;
-//  EXPECT_EQ((vecNd<2>{1,0}), (vecNd<2>{-1,0}*(-1.0))) ;
-//}
-
 class VecNdTest : public ::testing::Test {
   protected:
     class vecNd<3> vv000;
@@ -46,8 +33,6 @@ class VecNdTest : public ::testing::Test {
       vv_ex[0]=1;
       vv_ey[1]=1;
       vv_ez[2]=1;
-
-
     }
 };
 
@@ -73,115 +58,63 @@ TEST_F(VecNdTest, Basics002){
   EXPECT_DOUBLE_EQ(sqrt(2), abs(vv003)) ;
   EXPECT_DOUBLE_EQ(3, vv001*vv001) ;
   EXPECT_EQ(vv_ez, cross(vv_ex, vv_ey)) ;
+  EXPECT_EQ(vv_ex, cross(vv_ey, vv_ez)) ;
+  EXPECT_EQ(vv_ey, cross(vv_ez, vv_ex)) ;
+  EXPECT_DOUBLE_EQ(-0.5, (vv002*(vv_ex+vv_ey))) ;
 }
 
-//int main(){
-//
-//    //without argument zero vector is made    
-//    class vecNd<3> vv10;
-//    std::cout << "vv10:" << vv10             <<std::endl;
-//
-//    //with one double argument, all components will be initialized by the value specified.
-//    class vecNd<3> vv11(1.0);
-//    class vecNd<3> vv12(2.0);
-//
-//    //You can provde the double array to give arbitral initial values.
-//    double darray1[3] = {0.5, 1, 2.0};
-//    class vecNd<3> vv13(darray1);
-//    class vecNd<3> vv14 {1,2,100};
-//
-//    // the same applies for different dimensions. 
-//    class vecNd<4> vv21(1.0);
-//    class vecNd<4> vv22(2.0);
-//    double darray2[4] = {0.5, 1, 2.0, -1.0};
-//    class vecNd<4> vv23(darray2);
-//
-//    class matNd<3> I3;
-//    class matNd<4> I4;
-//
-//    class matNd<4> I5;
-//    I5=I4;
-//
-//
-//    // examples of vector calculations
-//    std::cout <<std::fixed;
-//    std::cout << "vecNd<VDIM>"              <<std::endl;
-//    std::cout << "VDIM =3"              <<std::endl;
-//    std::cout << "I4 == I4:" << (I4==I4)             <<std::endl;
-//    std::cout << "I4 == I4+I4:" << (I4 == (I4+I4))   <<std::endl;
-//    std::cout << "vv12:" << vv12             <<std::endl;
-//    std::cout << "vv11:" << vv11             <<std::endl;
-//    std::cout << "vv12:" << vv12             <<std::endl;
-//    std::cout << "vv13:" << vv13             <<std::endl;
-//    std::cout << "vv14:" << vv14             <<std::endl;
-//    std::cout << "+vv14:" << +vv14             <<std::endl;
-//    std::cout << "-vv14:" << -vv14             <<std::endl;
-//    std::cout << "vv13[0] :" << vv13[0]  <<std::endl;
-//    std::cout << "vv13[1] :" << vv13[1]  <<std::endl;
-//    std::cout << "vv13[2] :" << vv13[2]  <<std::endl;
-//    std::cout << "Inner-product"              <<std::endl;
-//    std::cout << " vv13*vv12     :" << vv13*vv12        <<std::endl;
-//    std::cout << " dot(vv13,vv12):" << dot(vv13,vv12)   <<std::endl;
-//    std::cout << " vv13.dot(vv12):" << vv13.dot(vv12)   <<std::endl;
-//    std::cout << "abs"              <<std::endl;
-//    std::cout << " vv11.abs() :" << vv11.abs()      <<std::endl;
-//    std::cout << " abs(vv11)  :" << abs(vv11)        <<std::endl;
-//    std::cout << "(vv11+vv13):" << (vv11+vv13) <<std::endl;
-//    std::cout << "(vv11-vv13):" << (vv11-vv13) <<std::endl;
-//    std::cout << "(20.0*vv11*10.0):" << (20.0*vv11*10.0) <<std::endl;
-//    std::cout << "(vv11/10.0):" << (vv11/10.0) <<std::endl;
-//    std::cout << "(vv11+=vv13) :" << (vv11+=vv13)  <<std::endl;
-//    std::cout << "(vv11-=vv13) :" << (vv11-=vv13)  <<std::endl;
-//    std::cout << "(vv11*=10.0) :" << (vv11*=10.0)  <<std::endl;
-//    std::cout << "cross product (three dimension only)"          << std::endl;
-//    std::cout << "cross(vv13,vv12): " << cross(vv13,vv12) <<std::endl;
-//    std::cout << "***********************" <<std::endl;
-//
-//
-//    std::cout <<               std::endl;
-//    std::cout << "VDIM =4"              <<std::endl;
-//    std::cout << "vv21:" << vv21             <<std::endl;
-//    std::cout << "vv22:" << vv22             <<std::endl;
-//    std::cout << "vv23:" << vv23             <<std::endl;
-//    std::cout << "vv23[0] :" << vv23[0]  <<std::endl;
-//    std::cout << "vv23[1] :" << vv23[1]  <<std::endl;
-//    std::cout << "vv23[2] :" << vv23[2]  <<std::endl;
-//    std::cout << "vv23[3] :" << vv23[3]  <<std::endl;
-//    std::cout << "vv23.at(3) :" << vv23.at(3)  <<std::endl;
-//    std::cout << "Inner-product"              <<std::endl;
-//    std::cout << " vv23*vv22     :" << vv23*vv22        <<std::endl;
-//    std::cout << " dot(vv23,vv22):" << dot(vv23,vv22)   <<std::endl;
-//    std::cout << " vv13.dot(vv22):" << vv23.dot(vv22)   <<std::endl;
-//    std::cout << "abs"              <<std::endl;
-//    std::cout << " vv21.abs() :" << vv21.abs()      <<std::endl;
-//    std::cout << " abs(vv21)  :" << abs(vv21)        <<std::endl;
-//    std::cout << "(vv21+vv23):" << (vv21+vv23) <<std::endl;
-//    std::cout << "(vv21-vv23):" << (vv21-vv23) <<std::endl;
-//    std::cout << "(20.0*vv21*10.0):" << (20.0*vv21*10.0) <<std::endl;
-//    std::cout << "(vv21/10.0):" << (vv21/10.0) <<std::endl;
-//    std::cout << "(vv21+=vv23) :" << (vv21+=vv23)  <<std::endl;
-//    std::cout << "(vv21-=vv23) :" << (vv21-=vv23)  <<std::endl;
-//    std::cout << "(vv21*=10.0) :" << (vv21*=10.0)  <<std::endl;
-//    std::cout << "***********************" <<std::endl;
-//
-//
-//    // without initializer, the unit matrix will be made
-//    class matNd<3> mat11;
-//    // if one double value is supplied, all compoments will be initialized by the value.
-//    class matNd<3> mat12(1);
-//    // you can give double array[VDIM][VDIM] to make arbitraly initialization
-//    double init_array1[3][3]= {{0,1,1.2},{5.4,0,0.9},{2.4,1.3,0}};
-//    class matNd<3> mat13(init_array1);
-//    class matNd<3> mat14 {9,8,7,6,5,4,3,2,1};
-//
-//    class matNd<4> mat21;
-//    class matNd<4> mat22(1);
-//    //double init_array2[4][4]= {{-1,2,3,4},{5,-1,7,8},{9,10,0,12},{13,14,15,1}};
-//    double init_array2[4][4]= {{2,0,0,1},{0,5,1,0},{-2,0,4,0},{0,1,1,1}};
-//    class matNd<4> mat23(init_array2);
-//
-//
-//
+class MatNdTest : public ::testing::Test {
+  protected:
+    class matNd<3> mat11;
+    class matNd<3> mat12 = matNd<3>(1);
+    class matNd<3> mat13 {9,8,7,6,5,4,3,2,1};
+    class matNd<3> mat14 = matNd<3>(0.0);
+    virtual void SetUp(){
+    }
+};
+
+TEST_F(MatNdTest, Basics000_initialization){
+  EXPECT_DOUBLE_EQ(1.0, mat11[0][0]) ;
+  EXPECT_DOUBLE_EQ(0.0, mat11[0][1]) ;
+  EXPECT_DOUBLE_EQ(0.0, mat11[0][2]) ;
+  EXPECT_DOUBLE_EQ(0.0, mat11[1][0]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat11[1][1]) ;
+  EXPECT_DOUBLE_EQ(0.0, mat11[1][2]) ;
+  EXPECT_DOUBLE_EQ(0.0, mat11[2][0]) ;
+  EXPECT_DOUBLE_EQ(0.0, mat11[2][1]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat11[2][2]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[0][0]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[0][1]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[0][2]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[1][0]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[1][1]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[1][2]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[2][0]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[2][1]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat12[2][2]) ;
+  EXPECT_DOUBLE_EQ(9.0, mat13[0][0]) ;
+  EXPECT_DOUBLE_EQ(8.0, mat13[0][1]) ;
+  EXPECT_DOUBLE_EQ(7.0, mat13[0][2]) ;
+  EXPECT_DOUBLE_EQ(6.0, mat13[1][0]) ;
+  EXPECT_DOUBLE_EQ(5.0, mat13[1][1]) ;
+  EXPECT_DOUBLE_EQ(4.0, mat13[1][2]) ;
+  EXPECT_DOUBLE_EQ(3.0, mat13[2][0]) ;
+  EXPECT_DOUBLE_EQ(2.0, mat13[2][1]) ;
+  EXPECT_DOUBLE_EQ(1.0, mat13[2][2]) ;
+}
+
+TEST_F(MatNdTest, Basics001){
+  EXPECT_DOUBLE_EQ(-9.0, (-mat13)[0][0]) ;
+  EXPECT_DOUBLE_EQ(-8.0, (-mat13)[0][1]) ;
+  EXPECT_DOUBLE_EQ(-7.0, (-mat13)[0][2]) ;
+  EXPECT_DOUBLE_EQ(-6.0, (-mat13)[1][0]) ;
+  EXPECT_DOUBLE_EQ(-5.0, (-mat13)[1][1]) ;
+  EXPECT_DOUBLE_EQ(-4.0, (-mat13)[1][2]) ;
+  EXPECT_DOUBLE_EQ(-3.0, (-mat13)[2][0]) ;
+  EXPECT_DOUBLE_EQ(-2.0, (-mat13)[2][1]) ;
+  EXPECT_DOUBLE_EQ(-1.0, (-mat13)[2][2]) ;
+  EXPECT_EQ(mat14, mat13-mat13) ;
+}
 //    std::cout << std::endl;
 //    std::cout << std::endl;
 //    std::cout << "***************************"              <<std::endl;
